4d0157dfebc0f4ef49810118292125003f88d11d
Nagender211 <dnagender2019@gmail.com>
2024-10-20 20:10:51 +0530

    ProductFilltGgroup

--- task/src/componets/ProductsFillterDeatils/AllProducts.js
+++ task/src/componets/ProductsFillterDeatils/AllProducts.js
@@ -9,16 +9,18 @@ const AllProducts = (props) => {
     }
   return (
     <div>
-     <Link to={`/product-details/${id}`}>
+    
       <h4>brand: {brand}</h4>
       <img src={imageUrl} alt={title} />
       <h5>id: {id}</h5>
+      <Link to={`/product-details/${id}`}>
 
       <h5 onClick={handleClick}>title: {title}</h5>
+      </Link>
       <h5>rating: {rating}</h5>
 
       <h5>price: {price}</h5>
-      </Link>
+    
 
     </div>
   )
