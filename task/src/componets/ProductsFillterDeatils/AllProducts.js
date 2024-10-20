5265e08088987ef3268d10d5dc27d022acfebf9f
Nagender211 <dnagender2019@gmail.com>
2024-10-20 17:25:31 +0530

    ProductFilltGgroup

--- task/src/componets/ProductsFillterDeatils/AllProducts.js
+++ task/src/componets/ProductsFillterDeatils/AllProducts.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const AllProducts = (props) => {
     const {product}=props;
-    const {brand,id,imageUrl,price,title}=product
+    const {brand,id,imageUrl,price,title,rating}=product
   return (
     <div>
-      all produts
+     
       <h4>brand: {brand}</h4>
       <img src={imageUrl} alt={title} />
       <h5>id: {id}</h5>
       <h5>title: {title}</h5>
+      <h5>rating: {rating}</h5>
+
       <h5>price: {price}</h5>
 
     </div>
